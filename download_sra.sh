55d42773f229c4546c12d80c68fc0264bbd9e498
Ronald Moura <48760028+ronaldmoura1989@users.noreply.github.com>
2025-03-13 17:13:30 -0300

    Update download_sra.sh

--- download_sra.sh
+++ download_sra.sh
@@ -1,13 +1,14 @@
 ncores=$(cat /proc/cpuinfo | grep processor | wc -l)
 memory_mega=$(free -m | awk -F" " 'NR>1 {print $2}' | sed -n 1p)
 
-samples=$(cat ./samples/GSE185919.csv | awk -F"," 'NR>1 {print $1}')
+samples=$(cat ./GSE185919.csv | awk -F"," 'NR>1 {print $1}')
 
 for (id in $samples); do
 
+    prefetch $id
     fasterq-dump --progress --threads $ncores --mem $memory_mega $id
-    pigz $id".fastq"
     pigz $id"_1.fastq"
     pigz $id"_2.fastq"
+    rm -r $id
 
 done
